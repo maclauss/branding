8562450e3ebdc2c01270af31a8c8ce52748774ef
Maxime CLAUSS <maxime.clauss@deepgreen.ai>
2018-12-26 13:57:27 +0700

    Enable contact form

--- src/main/js/public/ContactPage.js
+++ src/main/js/public/ContactPage.js
@@ -1,103 +1,82 @@
-// react imports
 import React from 'react';
-import PropTypes from 'prop-types';
 
-// material-ui imports
-import Grid from '@material-ui/core/Grid';
-import Hidden from '@material-ui/core/Hidden';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
+import ContactForm from './ContactForm';
 
-// deepgreen imports
+class ContactPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      sending: false,
+      sent: false,
+      firstName: { "value": "", "hasError": false },
+      lastName: { "value": "", "hasError": false },
+      email: { "value": "", "hasError": false },
+      subject: { "value": "", "hasError": false },
+      message: { "value": "", "hasError": false },
+    };
+  }
 
-const ContactPage = props =>
-  <div className={props.classes.heroUnit}>
-    <div className={props.classes.heroContent}>
-      <Typography variant="display2" align="center" color="textPrimary" gutterBottom>
-        Contact
-      </Typography>
-      <Typography variant="body2" align="center" color="textSecondary" paragraph>
-        Want to get in touch? Send me an email!
-      </Typography>
-      <div className={props.classes.heroButtons}>
-        <TextField
-          label="First Name"
-          className={props.classes.firstNameField}
-          type="text"
-          name="firstName"
-          autoComplete="given-name"
-          margin="normal"
-          variant="filled"
-        />
-        <TextField
-          label="Last Name"
-          className={props.classes.lastNameField}
-          type="text"
-          name="lastName"
-          autoComplete="family-name"
-          margin="normal"
-          variant="filled"
-        />
-        <TextField
-          label="Email"
-          type="email"
-          name="email"
-          autoComplete="email"
-          margin="normal"
-          variant="filled"
-          fullWidth
-        />
-        <TextField
-          label="Subject"
-          type="text"
-          name="subject"
-          margin="normal"
-          variant="filled"
-          fullWidth
-        />
-        <TextField
-          label="Message"
-          name="message"
-          margin="normal"
-          variant="filled"
-          fullWidth
-          multiline={true}
-          rows={4}
-          rowsMax={16}
-        />
-        <Button variant="contained" color="primary" disabled>
-          Coming Soon
-        </Button>
-      </div>
-    </div>
-  </div>;
+  change(stateKey, stateValue, error=false){
+    let { email, subject, message } = this.state;
+    this.setState({
+                    [stateKey]: { "value": stateValue, "hasError": error },
+                    sent: false
+                  });
+  }
 
-ContactPage.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+  submit(){
+    let { sending, sent, firstName, lastName, email, subject, message } = this.state;
+    if ( sending || sent ) return;
+    let re = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
+    if ( !re.test(email.value) ){
+      email = { value: email.value, hasError: true }
+      this.setState({ email: email });
+    }
+    if ( !subject.value ){
+      subject = { value: subject.value, hasError: true }
+      this.setState({ subject: subject });
+    }
+    if ( !message.value ){
+      message = { value: message.value, hasError: true }
+      this.setState({ message: message });
+    }
+    if ( !email.hasError && !subject.hasError && !message.hasError ){
+      this.setState({ sending: true });
+      $.ajax({
+          url: '/contact',
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify({
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,
+            subject: subject.value,
+            message: message.value
+          }),
+          success: (response) => {
+              this.setState({ sent: true, sending: false });
+          },
+          error: (jqXHR, errorStatus) => this.setState({ sending: false })
+      });
+    }
+  }
 
-const styles = theme => ({
-  heroUnit: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  heroContent: {
-    maxWidth: 600,
-    margin: '0 auto',
-    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
-  },
-  heroButtons: {
-    marginTop: theme.spacing.unit * 4,
-  },
-  firstNameField: {
-    marginRight: "5%",
-    width: "45%"
-  },
-  lastNameField: {
-    marginLeft: "5%",
-    width: "45%"
-  },
-});
+  render(){
+    const { sending, sent, firstName, lastName, email, subject, message } = this.state
+    return (
+      <ContactForm firstName={firstName}
+                   lastName={lastName}
+                   email={email}
+                   subject={subject}
+                   message={message}
+                   sending={sending}
+                   sent={sent}
+                   onChange={this.change.bind(this)}
+                   onSubmit={this.submit.bind(this)}/>
+    )
+  }
+}
 
-export default withStyles(styles)(ContactPage);
+ContactPage.propTypes = {};
+
+export default ContactPage;
